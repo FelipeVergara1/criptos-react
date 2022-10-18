d53ddac31213f1ae0470a480044f489d7892d1dc
FelipeVergara1 <fvergaragonzalez29@gmail.com>
2022-10-18 17:32:53 -0300

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,9 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar Estadounidense'},
+    {id: 'MXN', nombre: 'Peso Mexicano'},
+    {id: 'CLP', nombre: 'Peso Chileno'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'}
+]
+
+export {monedas}
